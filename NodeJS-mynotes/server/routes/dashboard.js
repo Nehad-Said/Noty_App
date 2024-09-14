e31e7bce6e0027af70eda99c97c11e7f27441e4c
Nehad-Said <nehadabasit@gmail.com>
2024-09-14 18:08:19 +0300

    still one error

--- NodeJS-mynotes/server/routes/dashboard.js
+++ NodeJS-mynotes/server/routes/dashboard.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboard_controller = require('../controllers/dashboard_controller');
 
 
@@ -7,9 +8,13 @@ const dashboard_controller = require('../controllers/dashboard_controller');
 * Dasboard routes
 */
 
-router.get('/dashboard', dashboard_controller.dashboard);
-
-
-
+router.get('/dashboard', isLoggedIn,  dashboard_controller.dashboard);
+router.get('/dashboard/item/:id', isLoggedIn,  dashboard_controller.dashboardViewNote);
+router.put('/dashboard/item/:id', isLoggedIn,  dashboard_controller.dashboardUpdateNote);
+router.delete('/dashboard/item-delete/:id', isLoggedIn,  dashboard_controller.dashboardDeleteNote);
+router.get('/dashboard/add', isLoggedIn,  dashboard_controller.dashboardAddNote);
+router.post('/dashboard/add', isLoggedIn,  dashboard_controller.dashboardAddNoteSubmit);
+router.get('/dashboard/search', isLoggedIn,  dashboard_controller.dashboardSearch);
+router.post('/dashboard/search', isLoggedIn,  dashboard_controller.dashboardSearchSubmit);
 
 module.exports = router;
